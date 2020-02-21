f77058a56da819d9d61cb4592e8e4ab4034eb315
kidys <arsenal.fmfan@gmail.com>
2020-02-22 00:38:37 +0300

    4 commit

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
+session_start();
+chdir(dirname(__DIR__));
+
+## Initializing the application
+
+/**
+ * The function of determining the language of the client
+ *
+ * @param string $default
+ * @return string
+ */
+function getLang($default = 'en') : string {
+    if (!empty($_GET['lang'])) return $_GET['lang'];
+    else if (!empty($_COOKIE['lang'])) return $_COOKIE['lang'];
+    else if (!empty($_SESSION['lang'])) return $_SESSION['lang'];
+    else if (!empty($_SERVER['HTTP_ACCEPT_LANGUAGE']))
+        return substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    else return $default;
+}
+
+## Running the application
+
+$lang = getLang();
 $name = $_GET['name'] ?: 'Guest';
-header('X-Developer: Denis Kitaev');
-echo 'Hello, ' . $name;
+echo 'Hello, ' . $name . '! Your lang is "' . $lang . '"';
